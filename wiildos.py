2d70faeb51dbbefeeab9fe97b3af44cbd1935b2f
Andrea Colangelo <warp10@debian.org>
2013-12-20 16:01:13 +0000

    Query upstream_version too

--- wiildos.py
+++ wiildos.py
@@ -8,14 +8,13 @@ cursor = conn.cursor()
 wiildos_pkgs_list = ('python-whiteboard', 'curtain', 'spotlighter', 'ardesia', 'epoptes', 'cellwriter', 'gnome-orca', 'gpicview', 'xournal', 'dia', 'inkscape', 'librecad', 'pinta', 'scribus', 'geany', 'scratch', 'kicad', 'osmo', 'pdfmod', 'freeplane', 'fbreader', 'ocrfeeder', 'tuxpaint', 'collatinus', 'geogebra', 'tuxmath', 'wxmaxima', 'lybniz', 'celestia', 'chemtool', 'gperiodic', 'stellarium', 'gnome-chemistry-utils', 'gcompris', 'jclic', 'numptyphysics', 'pingus', 'musescore', 'marble', 'florence')
 
 for pkg in wiildos_pkgs_list:
-
-    query = "SELECT ubuntu_sources.version, sources.version FROM ubuntu_sources INNER JOIN sources ON ubuntu_sources.source=sources.source WHERE ubuntu_sources.source='" + pkg + "' AND ubuntu_sources.release='trusty' AND sources.release='sid';"
+    query = "SELECT ubuntu_sources.version, sources.version, upstream.upstream_version FROM ubuntu_sources INNER JOIN sources ON ubuntu_sources.source=sources.source INNER JOIN upstream ON ubuntu_sources.source=upstream.source WHERE ubuntu_sources.source='" + pkg + "' AND ubuntu_sources.release='trusty' AND sources.release='sid';"
     cursor.execute(query)
 
-    print "pkg, ubuntu, debian"
+    print "pkg, ubuntu, debian, upstream"
     for row in cursor.fetchall():
-        ubuntu_version, debian_version = row
-        print pkg, ubuntu_version, debian_version
+        ubuntu_version, debian_version, upstream_version = row
+        print pkg, ubuntu_version, debian_version, upstream_version
 
 #SELECT architecture FROM packages WHERE source='scribus' AND version='1.4.2.dfsg+r18267-1' AND architecture IN ('amd64', 'i386') AND release='sid';
 # Query direttamente su packages?
