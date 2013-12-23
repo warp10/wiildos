33ede318a2bb47c57801a3f8e67a265fe5ff66e5
Andrea Colangelo <warp10@debian.org>
2013-12-23 17:09:24 +0000

    Improve the source package query

--- wiildos.py
+++ wiildos.py
@@ -7,14 +7,15 @@ cursor = conn.cursor()
 
 wiildos_pkgs_list = ('python-whiteboard', 'curtain', 'spotlighter', 'ardesia', 'epoptes', 'cellwriter', 'gnome-orca', 'gpicview', 'xournal', 'dia', 'inkscape', 'librecad', 'pinta', 'scribus', 'geany', 'scratch', 'kicad', 'osmo', 'pdfmod', 'freeplane', 'fbreader', 'ocrfeeder', 'tuxpaint', 'collatinus', 'geogebra', 'tuxmath', 'wxmaxima', 'lybniz', 'celestia', 'chemtool', 'gperiodic', 'stellarium', 'gnome-chemistry-utils', 'gcompris', 'jclic', 'numptyphysics', 'pingus', 'musescore', 'marble', 'florence')
 
-for pkg in wiildos_pkgs_list:
-    query = "SELECT ubuntu_sources.version, sources.version, upstream.upstream_version FROM ubuntu_sources INNER JOIN sources ON ubuntu_sources.source=sources.source INNER JOIN upstream ON ubuntu_sources.source=upstream.source WHERE ubuntu_sources.source='" + pkg + "' AND ubuntu_sources.release='trusty' AND sources.release='sid';"
+UBUNTU_RELEASE = 'trusty'
+DEBIAN_RELEASE = 'sid'
+
+for src_pkg in wiildos_pkgs_list:
+    query = "SELECT ubuntu_sources.version, sources.version, upstream.upstream_version, ubuntu_sources.bin, upstream.status FROM ubuntu_sources LEFT OUTER JOIN sources ON ubuntu_sources.source=sources.source LEFT OUTER JOIN upstream ON sources.source=upstream.source WHERE ubuntu_sources.source='" + src_pkg + "' AND ubuntu_sources.release='" + UBUNTU_RELEASE + "' AND sources.release='" + DEBIAN_RELEASE + "';"
     cursor.execute(query)
 
-    print "pkg, ubuntu, debian, upstream"
+    print "pkg_name, ubuntu_version, debian_version, upstream_version, ubuntu_bin, upstream_status"
     for row in cursor.fetchall():
-        ubuntu_version, debian_version, upstream_version = row
-        print pkg, ubuntu_version, debian_version, upstream_version
+        ubuntu_version, debian_version, upstream_version, ubuntu_bin, upstream_status = row
+        print src_pkg, ubuntu_version, debian_version, upstream_version, ubuntu_bin, upstream_status
 
-#SELECT architecture FROM packages WHERE source='scribus' AND version='1.4.2.dfsg+r18267-1' AND architecture IN ('amd64', 'i386') AND release='sid';
-# Query direttamente su packages?
