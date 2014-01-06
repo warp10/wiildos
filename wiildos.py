0402f06fddad517d8af2628fc4812752aa15ce2c
Andrea Colangelo <warp10@debian.org>
2014-01-06 12:05:28 +0000

    Some cleanup

--- wiildos.py
+++ wiildos.py
@@ -2,16 +2,20 @@
 
 import psycopg2
 
-conn = psycopg2.connect("service=udd")
-cursor = conn.cursor()
 
-wiildos_src_pkgs_list = ('python-whiteboard', 'curtain', 'spotlighter', 'ardesia', 'epoptes', 'cellwriter', 'gnome-orca', 'gpicview', 'xournal', 'dia', 'inkscape', 'librecad', 'pinta', 'scribus', 'geany', 'scratch', 'kicad', 'osmo', 'pdfmod', 'freeplane', 'fbreader', 'ocrfeeder', 'tuxpaint', 'collatinus', 'geogebra', 'tuxmath', 'wxmaxima', 'lybniz', 'celestia', 'chemtool', 'gperiodic', 'stellarium', 'gnome-chemistry-utils', 'gcompris', 'jclic', 'numptyphysics', 'pingus', 'musescore', 'marble', 'florence')
+wiildos_src_pkgs_list = ('python-whiteboard', 'curtain', 'spotlighter',
+                         'ardesia', 'epoptes', 'cellwriter', 'gnome-orca',
+                         'gpicview', 'xournal', 'dia', 'inkscape', 'librecad',
+                         'pinta', 'scribus', 'geany', 'scratch', 'kicad',
+                         'osmo', 'pdfmod', 'freeplane', 'fbreader',
+                         'ocrfeeder', 'tuxpaint', 'collatinus', 'geogebra',
+                         'tuxmath', 'wxmaxima', 'lybniz', 'celestia',
+                         'chemtool', 'gperiodic', 'stellarium',
+                         'gnome-chemistry-utils', 'gcompris', 'jclic',
+                         'numptyphysics', 'pingus', 'musescore', 'marble',
+                         'florence')
 UBUNTU_RELEASE = 'trusty'
 DEBIAN_RELEASE = 'sid'
-
-up_to_date = []
-newer_version_available = []
-other=[]
 
 
 def write(text, mode):
@@ -40,9 +44,25 @@ def write_footer():
 """
     write(footer, 'a')
 
+
 if __name__ == "__main__":
+    conn = psycopg2.connect("service=udd")
+    cursor = conn.cursor()
+
+    up_to_date = []
+    newer_version_available = []
+    other=[]
+
     for src_pkg in wiildos_src_pkgs_list:
-        query = "SELECT DISTINCT ubuntu_sources.source, ubuntu_sources.version, sources.version, upstream.upstream_version, upstream.status FROM ubuntu_sources LEFT OUTER JOIN sources ON ubuntu_sources.source=sources.source LEFT OUTER JOIN upstream ON sources.source=upstream.source WHERE ubuntu_sources.source='" + src_pkg + "' AND ubuntu_sources.release='" + UBUNTU_RELEASE + "' AND sources.release='" + DEBIAN_RELEASE + "';"
+        query = "SELECT DISTINCT ubuntu_sources.source, \
+                 ubuntu_sources.version, sources.version, \
+                 upstream.upstream_version, upstream.status \
+                 FROM ubuntu_sources LEFT OUTER JOIN sources \
+                 ON ubuntu_sources.source=sources.source LEFT OUTER JOIN \
+                 upstream ON sources.source=upstream.source \
+                 WHERE ubuntu_sources.source='" + src_pkg + \
+                 "' AND ubuntu_sources.release='" + UBUNTU_RELEASE + \
+                 "' AND sources.release='" + DEBIAN_RELEASE + "';"
         cursor.execute(query)
 
         for row in cursor.fetchall():  #fetchall() returns a list of tuples of strings, one tuple per match
