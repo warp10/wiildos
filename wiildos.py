0075afeebc9b1de575efbbf03059ba38b2448c82
Andrea Colangelo <warp10@debian.org>
2014-01-06 11:47:26 +0000

    Add check of __name__

--- wiildos.py
+++ wiildos.py
@@ -13,6 +13,7 @@ up_to_date = []
 newer_version_available = []
 other=[]
 
+
 def write(text, mode):
     with open('report.html', mode) as f:
         f.write(text)
@@ -27,8 +28,10 @@ def write_header():
         <h1> Wiildos Packages Health Status</h1>"""
     write(header, 'w+')
 
+
 def write_row():
     pass
+
 
 def write_footer():
     footer="""
@@ -37,25 +40,26 @@ def write_footer():
 """
     write(footer, 'a')
 
-for src_pkg in wiildos_src_pkgs_list:
-    query = "SELECT DISTINCT ubuntu_sources.source, ubuntu_sources.version, sources.version, upstream.upstream_version, upstream.status FROM ubuntu_sources LEFT OUTER JOIN sources ON ubuntu_sources.source=sources.source LEFT OUTER JOIN upstream ON sources.source=upstream.source WHERE ubuntu_sources.source='" + src_pkg + "' AND ubuntu_sources.release='" + UBUNTU_RELEASE + "' AND sources.release='" + DEBIAN_RELEASE + "';"
-    cursor.execute(query)
+if __name__ == "__main__":
+    for src_pkg in wiildos_src_pkgs_list:
+        query = "SELECT DISTINCT ubuntu_sources.source, ubuntu_sources.version, sources.version, upstream.upstream_version, upstream.status FROM ubuntu_sources LEFT OUTER JOIN sources ON ubuntu_sources.source=sources.source LEFT OUTER JOIN upstream ON sources.source=upstream.source WHERE ubuntu_sources.source='" + src_pkg + "' AND ubuntu_sources.release='" + UBUNTU_RELEASE + "' AND sources.release='" + DEBIAN_RELEASE + "';"
+        cursor.execute(query)
 
-    for row in cursor.fetchall():  #fetchall() returns a list of tuples of strings, one tuple per match
-        if row[-1] == 'up to date':
-            up_to_date.append(row)
-        elif row[-1] == 'Newer version available':
-            newer_version_available.append(row)
-        else:
-            other.append(row)
+        for row in cursor.fetchall():  #fetchall() returns a list of tuples of strings, one tuple per match
+            if row[-1] == 'up to date':
+                up_to_date.append(row)
+            elif row[-1] == 'Newer version available':
+                newer_version_available.append(row)
+            else:
+                other.append(row)
 
-write_header()
+    write_header()
 
-for row in other:
-    write_row()
-for row in newer_version_available:
-    write_row()
-for row in other:
-    write_row()
+    for row in other:
+        write_row()
+    for row in newer_version_available:
+        write_row()
+    for row in other:
+        write_row()
 
-write_footer()
+    write_footer()
